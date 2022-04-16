c9fcd626b174151d9fe3a7d7f76e057bc74cc84e
59418 <90524952+sj3697@users.noreply.github.com>
2022-04-16 15:07:46 -0700

    Add files via upload

--- BDM_HW3_sj3697.py
+++ BDM_HW3_sj3697.py
@@ -30,7 +30,7 @@ def main(sc):
                       .dropna(subset=['name'])
 
   
-  outputTask1.toJavaRDD.saveAsTextFile(sys.argv[2] if len(sys.argv)>2 else 'task1_output')
+  outputTask1.rdd.saveAsTextFile(sys.argv[2] if len(sys.argv)>2 else 'task1_output')
 
 
 
